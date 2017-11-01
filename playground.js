6dab5b676eb6a0a129ad38dd0f971697a86a140d
TheBrokenRail <connor24nolan@live.com>
2017-11-01 01:52:34 +0000

    Build for ae47c9f612df78dee6c9f5efe4f0f2bd0af619ec

--- playground.js
+++ playground.js
@@ -63301,7 +63301,7 @@ var PenMode = function (_React$Component) {
     }, {
         key: 'componentWillReceiveProps',
         value: function componentWillReceiveProps(nextProps) {
-            if (this.tool && (nextProps.colorState.strokeColor !== this.props.colorState.strokeColor || nextProps.colorState.strokeWidth !== this.props.colorState.strokeWidth)) {
+            if (this.tool && (nextProps.colorState.strokeColor !== this.props.colorState.strokeColor || nextProps.colorState.strokeWidth !== this.props.colorState.strokeWidth || nextProps.colorState.fillColor !== this.props.colorState.fillColor)) {
                 this.tool.setColorState(nextProps.colorState);
             }
 
